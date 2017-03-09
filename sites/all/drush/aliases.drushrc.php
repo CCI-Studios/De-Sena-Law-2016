da48420a69b7a46a32209283460abea30b8a6682
Chris Morris <cmorris@ccistudios.com>
2017-03-09 14:53:40 -0500

    updated drush aliases for a new staigng location

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -1,11 +1,22 @@
 <?php
 $aliases['dev'] = array(
-	'uri'=> 'staging.desenalaw.com',
-	'root' => '/home/desena/subdomains/staging/public_html',
+	'uri'=> 'desenalaw.ccistaging.com',
+	'root' => '/home/staging/subdomains/desenalaw/public_html',
 	'remote-host'=> 'host.ccistudios.com',
-	'remote-user'=> 'desena',
+	'remote-user'=> 'staging',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
 	),
 	'ssh-options'=>'-p 37241'
+);
+
+$aliases['olddev'] = array(
+        'uri'=> 'desenalaw.ccistaging.com',
+        'root' => '/home/desena/subdomains/staging/public_html',
+        'remote-host'=> 'host.ccistudios.com',
+        'remote-user'=> 'desena',
+        'path-aliases'=> array(
+                '%files'=> 'sites/default/files',
+        ),
+        'ssh-options'=>'-p 37241'
 );
